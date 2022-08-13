8318feeb40ffc0672e845e8e5df38a248d8ca89e
deysan <ideysan@gmail.com>
2022-08-13 23:08:45 +0300

    Update ticker animation

--- src/js/script.js
+++
@@ -1,7 +0,0 @@
-(function infinityTopBar(count = 20) {
-  const topBar = document.querySelector('.top-bar__ticker');
-  const topBarContent = document.querySelector('.top-bar__content');
-  for (i = 0; i < count - 1; i++) {
-    topBar.appendChild(topBarContent.cloneNode(true));
-  }
-})();
